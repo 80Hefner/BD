f97d2554feb6a28f19d62f2e70578d3839c2a9ff
Bruno Dias <47273656+Tourette137@users.noreply.github.com>
2020-12-04 16:18:16 +0000

    Add files via upload

---
+++ index.sql
@@ -0,0 +1,6 @@
+CREATE INDEX idx_nomeCli ON Cliente(Nome);
+CREATE INDEX idx_nomeFunc ON Funcionario(Nome);
+
+CREATE INDEX idx_desig ON Produto(Designacao);
+
+CREATE INDEX idx_data ON Fatura(dataFatura);
